6f4af421b7a18afa53786906b2d76d56a8f72b78
HMA <HMA@HMAP>
2015-04-25 12:56:57 -0700

    Merge origin/master
    Conflicts:
    	ServletProject/src/java/trainee/controller/uploadAssignment.java

--- TabeebkServer/src/main/java/com/tabeebkServer/pojo/Clinic.java
+++ TabeebkServer/src/main/java/com/tabeebkServer/pojo/Clinic.java
@@ -1,5 +1,5 @@
 package com.tabeebkServer.pojo;
-// Generated Apr 25, 2015 12:37:48 PM by Hibernate Tools 4.3.1
+// Generated Apr 25, 2015 12:56:11 PM by Hibernate Tools 4.3.1
 
 
 import java.util.HashSet;

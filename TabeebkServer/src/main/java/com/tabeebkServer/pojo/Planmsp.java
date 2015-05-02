2881808042545302ef8d55fe380cbe4d42af94ad
karim_i35_ewd15 <karim.abdelmohsen.1992@gmail.com>
2015-05-02 11:09:19 +0200

    Tabeebk Updates Admin Pages

--- TabeebkServer/src/main/java/com/tabeebkServer/pojo/Planmsp.java
+++ TabeebkServer/src/main/java/com/tabeebkServer/pojo/Planmsp.java
@@ -1,5 +1,5 @@
 package com.tabeebkServer.pojo;
-// Generated May 2, 2015 1:54:41 AM by Hibernate Tools 3.6.0
+// Generated May 2, 2015 11:08:35 AM by Hibernate Tools 3.6.0
 
 
 import javax.persistence.AttributeOverride;
